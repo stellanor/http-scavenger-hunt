fbb9db1885f99319608cfd64064953092d5bd37c
Ellie Poley <ellie.poley@devbootcamp.com>
2016-09-15 16:42:05 -0500

    Implement basic routes

--- app/controllers/levels.rb
+++ app/controllers/levels.rb
@@ -7,20 +7,28 @@ get level_path(0) do
 end
 
 get level_path(1) do
+  erb :'level_1'
 end
 
 get level_path(2) do
+  erb :'level_2'
 end
 
 get level_path(3) do
+  erb :'level_3'
 end
 
 get level_path(4) do
+  status 301
+  headers "Location" => "/whatsgoingon"
+  content_type :text
+  erb :'level_4', layout: false
 end
 
 get level_path(5) do
+  erb :'level_5'
 end
 
 get '/whatsgoingon' do
-
+  erb :'level_4_bounce'
 end
