8c79dbc10075bd82dbd6bdf7a3781daaab0dc7bc
Ellie Poley <ellie.poley@devbootcamp.com>
2016-09-14 19:06:24 -0500

    Mock in level routes, including a manifest of them to make the cryptic names easier to read

--- app/controllers/levels.rb
+++ app/controllers/levels.rb
@@ -1,3 +1,35 @@
-get '/' do
+def l(str)
+  '/levels/' + str
+end
+
+level_routes = [
+  '/',
+  l("one"),
+  l("2"),
+  l("iii"),
+  l("quatro"),
+  l("00000101")
+]
+
+get level_routes[0] do
   erb :'welcome'
+end
+
+get level_routes[1] do
+end
+
+get level_routes[2] do
+end
+
+get level_routes[3] do
+end
+
+get level_routes[4] do
+end
+
+get level_routes[5] do
+end
+
+get '/whatsgoingon' do
+
 end
