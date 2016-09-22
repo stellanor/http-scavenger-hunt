049e5864b65e17e845615fb2af976ef66e3cde84
Matt Baker <mbaker.pdx@gmail.com>
2016-09-22 09:55:09 -0500

    add uniqueness validation on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # require 'bcrypt'
 
 class User < ActiveRecord::Base
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 end
